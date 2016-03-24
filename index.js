bb24b5da5dc579987318e0afb3c0e76a0d3bb3df
Joe Dursun <joedursun@gmail.com>
2016-03-24 14:50:12 -0400

    Put in SIGINT handler for clean exit

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var express = require('express'),
     phantom = require('phantom'),
     PORT = 8080;
 
+// stupid hack for Docker not exiting cleanly
+process.on('SIGINT', function() {
+  process.exit();
+});
+
 function screenCap(resourceLocation, resultType) {
   var result;
   phantom.create()
