221a0d21fa5ab503ab03551aad32e5fee21ea34c
Joe Dursun <joedursun@gmail.com>
2016-03-25 11:28:27 -0400

    Render files or base64 encoded image strings

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var express = require('express'),
-    app = express(),
+    tmp = require('tmp'),
     phantom = require('phantom'),
-    exec = require('child_process').exec,
+    fs = require('fs'),
+    childProcess = require('child_process');
+
+var app = express(),
     PORT = 8080;
 
 // stupid hack for Docker not exiting cleanly
@@ -9,27 +12,50 @@ process.on('SIGINT', function() {
   process.exit();
 });
 
-function screenCap(resourceLocation, resultType, callback) {
-  var result, screenCapScript, cmd;
-
-  if(resultType === 'pdf') {
-    screenCapScript = '/src/pdf.js';
-  } else if(['png', 'jpg', 'jpeg', 'gif'].indexOf(resultType) > -1) {
-    screenCapScript = '/src/image.js';
+function screenCap(resourceLocation, resultType, response, responseFormat) {
+  if(responseFormat === 'string'){
+    screenCapToEncodedString(resourceLocation, response);
+  } else {
+    screenCapToFile(resourceLocation, resultType, response);
   }
+}
 
-  cmd = 'phantomjs ' + screenCapScript + ' ' + resourceLocation;
-  exec(cmd, function(error, stdout, stderr){
-    callback(stdout);
+function screenCapToEncodedString(resourceLocation, response) {
+  var cmd = 'phantomjs /src/render_encoded_string.js ' + resourceLocation;
+
+  childProcess.exec(cmd, function(error, stdout, stderr){
+    response.send(stdout);
+  });
+}
+
+function screenCapToFile(resourceLocation, resultType, response) {
+  tmp.file(function tmpFileCreated(err, path, fd, cleanupCallback){
+    if (err) throw err;
+    var cmd,
+        fileName = path.split('.')[0] + '.' + resultType;
+
+    cmd = 'phantomjs /src/render_file.js "' + resourceLocation + '" ' + fileName;
+
+    childProcess.exec(cmd, function(err, stdout, stderr){
+      response.sendFile(fileName, function (err){
+        if (err) {
+          console.log(err);
+          response.status(err.status).end();
+        }
+        fs.unlink(fileName, function(err){
+          if (err) throw err;
+        });
+      });
+    });
   });
 }
 
 app.get('/capture', function (req, res) {
   var resourceLocation = req.query.resource,
-      resultType = req.query.type;
-  screenCap(resourceLocation, resultType, function(base64img){
-    res.send(base64img);
-  });
+      resultType = req.query.type,
+      responseFormat = req.query.format || 'file';
+
+  screenCap(resourceLocation, resultType, res, responseFormat);
 });
 
 app.listen(PORT);
