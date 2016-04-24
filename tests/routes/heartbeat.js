9491b10c6d338e3059d724416fac6db0d474556a
Joe Dursun <joedursun@gmail.com>
2016-04-24 18:18:09 -0400

    Test the heartbeat route

---
+++ tests/routes/heartbeat.js
@@ -0,0 +1,8 @@
+var app = require('../../app');
+var request = require('supertest');
+
+describe('/heartbeat', function(){
+  it('returns "ok!" with a 200 status code', function(done){
+    request(app).get('/heartbeat').expect(200, 'ok!', done);
+  });
+});
