73537538a65ab23381afe0a83e7da4deeaa9afad
Joe Dursun <joedursun@gmail.com>
2016-04-25 09:48:16 -0400

    Fix YAML file loading & update url char whitelist

--- helpers/request_helper.js
+++ helpers/request_helper.js
@@ -3,7 +3,7 @@ var crypto = require('crypto');
 var fs = require('fs');
 var yaml = require('js-yaml');
 
-var appData = yaml.safeLoad('app_data', fs.readFileSync('/src/config/app_data.yml'));
+var appData = yaml.safeLoad(fs.readFileSync('/src/config/app_data.yml'));
 var appSecret = appData.app_secret;
 
 var log = function(req, res, next){
@@ -17,20 +17,19 @@ var digest = function(str){
   return sha256.digest('base64');
 }
 
-var verifySignature = function(req, res, next, appSecret) {
+var verifySignature = function(req, res, next) {
   var signature = req.query.signature,
       resource = req.query.resource;
 
   if (digest(resource + appSecret) === signature) {
     next();
   } else {
-    console.log(resource, appSecret, signature, digest(resource + appSecret));
     res.status(403).render('error', {message: 'Invalid signature.'});
   }
 }
 
 var check = function(req, res, next){
-  var whitelistRegexp = /\/capture\/?\?([A-z0-9=&\/]|\n)*$/;
+  var whitelistRegexp = /\/capture\/?\?([A-z0-9=&+\/]|\n)*$/;
 
   if (whitelistRegexp.test(req.originalUrl)) {
     next();
