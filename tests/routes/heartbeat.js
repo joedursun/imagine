2168fedb9c399368b9a7b6ea24581c693510482f
Joe Dursun <joedursun@gmail.com>
2016-06-14 15:15:34 +0300

    Use the test `done` callback properly

--- tests/routes/heartbeat.js
+++ tests/routes/heartbeat.js
@@ -3,6 +3,6 @@ var request = require('supertest');
 
 describe('/heartbeat', function(){
   it('returns "ok!" with a 200 status code', function(done){
-    request(app).get('/heartbeat').expect(200, 'ok!', done);
+    request(app).get('/heartbeat').expect(200, 'ok!', done());
   });
 });
